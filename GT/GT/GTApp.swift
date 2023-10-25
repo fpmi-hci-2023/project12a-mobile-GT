15d21cf54d2c3306db33594b11566fbc729e3a91
Uladz1slau <uladz1slau@mail.ru>
2023-10-25 12:30:35 +0300

    init

---
+++ GT/GT/GTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTApp.swift
+//  GT
+//
+//  Created by Vlad Muraveiko on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct GTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
